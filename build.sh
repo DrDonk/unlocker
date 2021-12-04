c048601d9e52c66db4f4498d2a43931e17d5f6bc
Dave Parsons <dave@daveparsons.net>
2021-12-04 17:37:05 +0000

    Binaries from testing build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-env GOOS=windows GOARCH=amd64 go build ./tools/dumpsmc.go
-env GOOS=linux GOARCH=amd64 go build ./tools/dumpsmc.go
-env GOOS=windows GOARCH=amd64 go build ./command/unlocker.go
-env GOOS=linux GOARCH=amd64 go build ./command/unlocker.go
-zip ./build/release.zip dumpsmc dumpsmc.exe unlocker unlocker.exe unlocker.exe.manifest iso/*
+env GOOS=windows GOARCH=amd64 go build -o ./windows/dumpsmc.exe ./tools/dumpsmc.go
+env GOOS=windows GOARCH=amd64 go build -o ./windows/unlocker.exe ./command/unlocker.go
+env GOOS=linux GOARCH=amd64 go build -o ./linux/dumpsmc ./tools/dumpsmc.go
+env GOOS=linux GOARCH=amd64 go build -o ./linux/unlocker ./command/unlocker.go
