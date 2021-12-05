464cccc7e95e301e7dd5ca7c963428942747ab8e
David Parsons <dave@daveparsons.net>
2021-12-05 16:36:15 +0000

    Updated Windows repaior script

--- windows/repair.ps1
+++ windows/repair.ps1
@@ -1,36 +1,4 @@
 $setup=$args[0]
 Write-Host "Repairing VMware installation from $setup"
-$installpath=Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\VMware Player' -Name InstallPath
-Write-Host "$installpath"
-$installpath64=Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\VMware Player' -Name InstallPath64
-Write-Host "$installpath64"
-Stop-Process -Name vmware-tray -Force -ErrorAction SilentlyContinue
-Stop-Service -Name VMAuthdService -Force
-(Get-Service VMAuthdService).WaitForStatus('Stopped')
-Stop-Service -Name VMUSBArbService -Force
-(Get-Service VMUSBArbService).WaitForStatus('Stopped')
-$service = Get-Service -Name VMwareHostd -ErrorAction SilentlyContinue
-if($service -ne $null)
-{
-    Stop-Service -Name VMwareHostd -Force
-    (Get-Service VMwareHostd).WaitForStatus('Stopped')
-}
-
-$vmwarevmx=Join-Path -Path $installpath64 -ChildPath "vmware-vmx.exe"
-Write-Host $vmwarevmx
-Remove-Item -Path $vmwarevmx -Force
-
-$vmwarevmxdebug=Join-Path -Path $installpath64 -ChildPath "vmware-vmx-debug.exe"
-Write-Host $vmwarevmxdebug
-Remove-Item -Path $vmwarevmxdebug -Force
-
-$vmwarevmxstats=Join-Path -Path $installpath64 -ChildPath "vmware-vmx-stats.exe"
-$isstats=Get-Item $vmwarevmxstats
-if($isstats -ne $null) {
-    Write-Host $vmwarevmxstats
-    Remove-Item -Path $vmwarevmxstats -Force
-}
-$vmwarebase=Join-Path -Path $installpath -ChildPath "vmwarebase.dll"
-Write-Host $vmwarebase
-Remove-Item -Path $vmwarebase -Force
-Start-Process -Wait $setup -ArgumentList "/s /v `"EULAS_AGREED=1 REINSTALLMODE=vomus /qn`""
+Start-Process -Wait msiexec.exe -ArgumentList "/x `"C:\Program Files (x86)\Common Files\VMware\InstallerCache\{9797B0AF-2570-4326-B9A8-C1B4E75DFFC0}.msi`" /qb"
+Start-Process -Wait "$setup" -ArgumentList "/s /v `"EULAS_AGREED=1 REINSTALLMODE=vomus /qb`""
