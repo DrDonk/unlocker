54015aa7c16a3ab65c03b5bb0cdefafe6b260b0c
David Parsons <dave@daveparsons.net>
2022-05-13 17:19:13 +0100

    Development: Remove unused files

--- tools/windows/repair.ps1
+++
@@ -1,4 +0,0 @@
-$setup=$args[0]
-Write-Host "Repairing VMware installation from $setup"
-Start-Process -Wait msiexec.exe -ArgumentList "/x `"C:\Program Files (x86)\Common Files\VMware\InstallerCache\{9797B0AF-2570-4326-B9A8-C1B4E75DFFC0}.msi`" /qb"
-Start-Process -Wait "$setup" -ArgumentList "/s /v `"EULAS_AGREED=1 REINSTALLMODE=vomus /qb`""
