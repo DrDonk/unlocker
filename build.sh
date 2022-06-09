683b04d389643ceb7844e27156c7eb0254d7b61e
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2022-06-09 15:23:55 +0100

    Fix build.sh output binaries
    The current binaries put into the `dist` directory build to `linux` and `windows` as the file names, and that gets overwritten each time you try to build from source. This ensures all the executables get outputted to the correct directories.

--- build.sh
+++ build.sh
@@ -10,24 +10,24 @@ fi
 pushd ./commands/check
 echo "Building check"
 go-winres make --arch amd64 --product-version $1 --file-version $1
-env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows
-env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux
+env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows/check.exe
+env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux/check
 rm rsrc_windows_amd64.syso
 popd
 
 pushd ./commands/relock
 echo "Building relock"
 go-winres make --arch amd64 --product-version $1 --file-version $1
-env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows
-env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux
+env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows/relock.exe
+env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux/relock
 rm rsrc_windows_amd64.syso
 popd
 
 pushd ./commands/unlock
 echo "Building unlock"
 go-winres make --arch amd64 --product-version $1 --file-version $1
-env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows
-env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux
+env GOOS=windows GOARCH=amd64 go build -o ../../dist/windows/unlock.exe
+env GOOS=linux GOARCH=amd64 go build -o ../../dist/linux/unlock
 rm rsrc_windows_amd64.syso
 popd
 
