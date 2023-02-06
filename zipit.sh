bddd4a9fd6df94ad6b52fd2a7fb75572663d6c0b
David Parsons <dave@daveparsons.net>
2023-02-06 17:26:07 +0000

    4.2.5: Print 256 and 512 shasum when building distribution files

--- zipit.sh
+++ zipit.sh
@@ -12,3 +12,8 @@ rm -vrf ./dist/unlocker$VERSION
 7z a ./dist/unlocker$VERSION.zip ./build/*
 tar czvf ./dist/unlocker$VERSION.tgz ./build/*
 7z x -o./dist/unlocker$VERSION ./dist/unlocker$VERSION.zip
+
+shasum -a 256  ./dist/unlocker$VERSION.tgz
+shasum -a 256  ./dist/unlocker$VERSION.zip
+shasum -a 512  ./dist/unlocker$VERSION.tgz
+shasum -a 512  ./dist/unlocker$VERSION.zip
